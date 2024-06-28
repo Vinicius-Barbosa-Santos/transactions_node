15dcd0cfc4a426a888795547d4f868715f55a657
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-06-28 19:57:48 -0300

    feat: implementing routes - creating transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,27 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
 import { knex } from "../database";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transactions = await knex("transactions")
-      .where("amount", 1000)
-      .select("*");
+  app.post("/", async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
 
-    return transactions;
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
+
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      // Se for crédito ele usa do mesmo valor, caso contrário ele cadastra o valor do amount como negativo, assim trazendo o valor total
+      amount: type === "credit" ? amount : amount * -1,
+    });
+
+    return reply.status(201).send();
   });
 }
