83a549a8db653d736e338dd93e79c26eb605900b
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-06-28 21:44:21 -0300

    feat: hook global

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,12 @@ import { transactionsRoutes } from "./routes/transactions";
 const app = fastify();
 
 app.register(cookie);
+
+// Se eu quiser que este hook global se aplique para todas as rotas preciso inserir no meu server.
+app.addHook("preHandler", async (request, reply) => {
+  console.log(`${request.method} ${request.url}`);
+});
+
 app.register(transactionsRoutes, {
   prefix: "transactions",
 });
